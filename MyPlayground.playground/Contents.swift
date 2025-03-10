a4dfdd033486107ee43094794f73425df34cd131
Aiden <kkt6394@gmail.com>
2025-03-10 21:58:14 +0900

    초안

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,45 @@
+protocol Operation {
+    func operate(a: Int, b: Int) -> Int
+}
+
+class Add: Operation {
+    func operate(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
+
+class Subtract: Operation {
+    func operate(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
+
+class Multiply: Operation {
+    func operate(a: Int, b: Int) -> Int {
+        return a * b
+    }
+}
+
+class Divide: Operation {
+    func operate(a: Int, b: Int) -> Int {
+        return b != 0 ? a / b : 0
+    }
+}
+
+class Remainder: Operation {
+    func operate(a: Int, b: Int) -> Int {
+        return a % b
+    }
+}
+
+let add: Operation = Add()
+let subtract: Operation = Subtract()
+let multiply: Operation = Multiply()
+let divide: Operation = Divide()
+let remainder: Operation = Remainder()
+
+print(add.operate(a: 10, b: 20))
+print(subtract.operate(a: 10, b: 20))
+print(multiply.operate(a: 10, b: 20))
+print(divide.operate(a: 10, b: 20))
+print(remainder.operate(a: 10, b: 20))
