a699e9b1dee2a420870eab723b515a7343955dc1
Aiden <kkt6394@gmail.com>
2025-03-10 22:07:36 +0900

    0으로 나누었을때, 나눗셈연산 및 나머지연산 오류방지 추가

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,33 +1,39 @@
 protocol Operation {
-    func operate(a: Int, b: Int) -> Int
+    func operate(a: Int, b: Int) -> Int?
 }
 
 class Add: Operation {
-    func operate(a: Int, b: Int) -> Int {
+    func operate(a: Int, b: Int) -> Int? {
         return a + b
     }
 }
 
 class Subtract: Operation {
-    func operate(a: Int, b: Int) -> Int {
+    func operate(a: Int, b: Int) -> Int? {
         return a - b
     }
 }
 
 class Multiply: Operation {
-    func operate(a: Int, b: Int) -> Int {
+    func operate(a: Int, b: Int) -> Int? {
         return a * b
     }
 }
 
 class Divide: Operation {
-    func operate(a: Int, b: Int) -> Int {
-        return b != 0 ? a / b : 0
+    func operate(a: Int, b: Int) -> Int? {
+        guard b != 0 else {
+            return nil
+        }
+        return a / b
     }
 }
 
 class Remainder: Operation {
-    func operate(a: Int, b: Int) -> Int {
+    func operate(a: Int, b: Int) -> Int? {
+        guard b != 0 else {
+            return nil
+        }
         return a % b
     }
 }
